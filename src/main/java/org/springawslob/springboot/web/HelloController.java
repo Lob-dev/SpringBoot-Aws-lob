5c333034dffe281ee422cfa438178a8dc1aa6f38
DESKTOP-Q7U1K2V\serrl <coffeescript@kakao.com>
2020-08-11 18:10:28 +0900

    helloDto 추가

--- src/main/java/org/springawslob/springboot/web/HelloController.java
+++ src/main/java/org/springawslob/springboot/web/HelloController.java
@@ -1,6 +1,9 @@
 package org.springawslob.springboot.web;
 
+import lombok.RequiredArgsConstructor;
+import org.springawslob.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -15,6 +18,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloResponseDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
     }
 
 }
