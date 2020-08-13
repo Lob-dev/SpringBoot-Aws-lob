f4155b0133571d23e70fcf3f6b16d0a7c52a78c5
DESKTOP-Q7U1K2V\serrl <coffeescript@kakao.com>
2020-08-13 15:57:56 +0900

    3.4 반영

---
+++ src/main/java/org/springawslob/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package org.springawslob.springboot.web.dto;
+
+import lombok.Getter;
+import org.springawslob.springboot.domain.posts.Posts;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
