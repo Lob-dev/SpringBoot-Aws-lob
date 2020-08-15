2faa428fac463e475ce415df996f7a60cc5899ee
DESKTOP-Q7U1K2V\serrl <coffeescript@kakao.com>
2020-08-15 23:57:51 +0900

    4.1 추가

--- src/main/java/org/springawslob/springboot/web/HelloController.java
+++ src/main/java/org/springawslob/springboot/web/HelloController.java
@@ -10,11 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
-    public String root() {
-        return "index.html";
-    }
-
     @GetMapping("/hello")
     public String hello() {
         return "hello";
